c1ec450c946bda5b92d50827db105f0dc039afc6
olpos <jasperOlpos@gmail.com>
2025-02-18 12:40:15 +0800

    Update index.php

--- practice-3/index.php
+++ practice-3/index.php
@@ -5,4 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Even Numbers from Matrix</title>
 </head>
-<body></body>
+<body> 
+
+
+<h2>Even Numbers from Matrix</h2>
+
+</body>
