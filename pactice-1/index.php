819d07db61c642651b9ce5cd75224a98609a95bb
olpos <jasperOlpos@gmail.com>
2025-02-18 11:54:51 +0800

    Update index.php

--- pactice-1/index.php
+++ pactice-1/index.php
@@ -8,3 +8,10 @@
 <body>
 
 <h2>Triangle Area Calculator</h2>
+
+<form method="POST">
+    Side 1: <input type="number" step="any" name="side1" required><br><br>
+    Side 2: <input type="number" step="any" name="side2" required><br><br>
+    Side 3: <input type="number" step="any" name="side3" required><br><br>
+    <button type="submit" name="calculate">Calculate Area</button>
+</form>
